a5e1f5476a44b2293ca08e33695999ae932484b8
MinhTamPhan <phanminhtam247@gmail.com>
2020-09-12 23:25:05 +0700

    add baseline2

--- sorting_gpu/src/helper.cuh
+++ sorting_gpu/src/helper.cuh
@@ -63,4 +63,38 @@ void sortByThrust(const uint32_t * in, int n, uint32_t * out, int blockSize) {
     thrust::sort(dv_out.begin(), dv_out.end());
     thrust::copy(dv_out.begin(), dv_out.end(), out);
 }
+
+
+void printDeviceInfo() {
+    cudaDeviceProp devProv;
+    CHECK(cudaGetDeviceProperties(&devProv, 0));
+    printf("**********GPU info**********\n");
+    printf("Name: %s\n", devProv.name);
+    printf("Compute capability: %d.%d\n", devProv.major, devProv.minor);
+    printf("Num SMs: %d\n", devProv.multiProcessorCount);
+    printf("Max num threads per SM: %d\n", devProv.maxThreadsPerMultiProcessor); 
+    printf("Max num warps per SM: %d\n", devProv.maxThreadsPerMultiProcessor / devProv.warpSize);
+    printf("GMEM: %zu byte\n", devProv.totalGlobalMem);
+    printf("SMEM per SM: %zu byte\n", devProv.sharedMemPerMultiprocessor);
+    printf("SMEM per block: %zu byte\n", devProv.sharedMemPerBlock);
+    printf("****************************\n");
+}
+
+void checkCorrectness(uint32_t * out, uint32_t * correctOut, int n) {
+    for (int i = 0; i < n; i++) {
+        if (out[i] != correctOut[i]) {
+            printf("INCORRECT :(\n");
+            return;
+        }
+    }
+    printf("CORRECT :)\n");
+}
+
+void printArray(uint32_t * a, int n) {
+    for (int i = 0; i < n; i++)
+        printf("%i ", a[i]);
+    printf("\n");
+}
+
+
 #endif
